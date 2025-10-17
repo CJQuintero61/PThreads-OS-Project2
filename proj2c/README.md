5a5dd98fd349c563a1ae91e1411f9c85749d9498
CJQuintero61 <cjquintero61@gmail.com>
2025-10-17 10:05:44 -0500

    setup project part c

---
+++ proj2c/README.md
@@ -0,0 +1,18 @@
+### proj2c.c
+
+This directory contains the source code, the makefile, and the readme for 
+project 2 part c. This program gets an input string from the user then makes 2 threads 
+to prind the output where one thread is responsible for printing the words in the input 
+string that start with a consonant and the other printing the words that start with a
+vowel. 
+
+To Run:
+  1. must be in a unix based terminal or wsl
+  2. `make` to compile
+  3. `make run` to execute the program
+  4. follow any user prompts if applicable
+  5. `make clean` to remove the compiled files
+
+
+Fall 2025 Christian Quintero
+
---
+++ proj2c/proj2c.c
@@ -0,0 +1,29 @@
+/*
+	File: proj2cc.c
+	Created: 10/17/2025 by Christian Quintero
+	Last Updated: 10/17/2025 by Christian Quintero
+
+	Description:
+    This program gets an input string from the user then makes 2 threads 
+    to prind the output where one thread is responsible for printing the words in the input 
+    string that start with a consonant and the other printing the words that start with a
+    vowel. 
+  
+    To Run:
+    1. must be in a unix based terminal or wsl
+    2. `make` to compile
+    3. `make run` to execute the program
+    4. follow the input prompts. 
+    5. `make clean` to remove the compiled files
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+
+int main() {
+
+    
+    return 0;
+} // end main
